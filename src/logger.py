6d3795a6ce2908781e3236fdaad6c711480a0dbe
Param Pathak <param.pathak07@gmail.com>
2023-08-03 14:46:46 +0530

    changes

--- src/logger.py
+++ src/logger.py
@@ -13,12 +13,3 @@ logging.basicConfig(
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
-
-
-if __name__ == "__main__":
-
-    try:
-        a=1/0
-    except:
-        logging.info("Logging has started")
-        raise CustomException 
